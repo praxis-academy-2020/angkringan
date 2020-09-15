1100a1d27be13393b19e77ee73870bc0eb61119b
Jembar <jembar.ashofa01@gmail.com>
2020-09-15 10:04:36 +0700

    add files

--- angkringan/src/router/index.js
+++ angkringan/src/router/index.js
@@ -11,13 +11,18 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/minuman",
+    name: "minuman",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/snack",
+    name: "snack",
+    component: () => import(/* webpackChunkName: "snack" */ "../components/HelloWorld.vue")
   }
 ];
 
