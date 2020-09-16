9fc1fc0d4848343a88ea2f6bb4fa353ef6b156ac
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2020-09-16 10:50:18 +0700

    file tambahan

--- angkringan/src/views/Makanan.vue
+++ angkringan/src/views/Makanan.vue
@@ -1,26 +1,26 @@
 <template>
-    <div>
-        <h2>{{ dataMakanan }}</h2>
-        <h1>ini maknaan</h1>
+  <div>
+    <h1>saya makan</h1>
+    <div v-for="n in dataMakanan" :key="n.id">
+      <div v-for="a in n" :key="a.id">
+        {{ a.item }} || {{ a.harga }} || {{ a.deskripsi }}
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "Makanan",
-    data() {
-        return {
-            makananList: []
-        }
-    },
-    methods: {
-        
-        },
-    computed: {
-        
-        dataMakanan: function() {
-            console.log(this.$store.getters.getData);
-            return this.$store.getters.getData;
-        }
-    },
-}
+  name: "Makanan",
+  data() {
+    return {
+      makananList: []
+    };
+  },
+  methods: {},
+  computed: {
+    dataMakanan() {
+      return this.$store.getters.getData;
+    }
+  }
+};
 </script>
--- angkringan/src/views/Minuman.vue
+++ angkringan/src/views/Minuman.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>
-        
+  <div>
+    <h1>halaman Minum</h1>
+    <div v-for="n in dataMinum" :key="n.id">
+      <div v-for="a in n" :key="a.id">
+        <p>{{ a.item }} || {{ a.harga }} || {{ a.deskripsi }}</p>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  name: "Minuman",
+  data() {
+    return {
+      minumanList: []
+    };
+  },
+  methods: {},
+  computed: {
+    dataMinum() {
+      return this.$store.getters.getDataM;
+    }
+  }
+};
 </script>
